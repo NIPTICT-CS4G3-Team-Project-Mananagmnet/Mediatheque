b258434a514db12c906389846cb2850091942d8a
MakRatanakKh <mak.ratanak.kh@gmail.com>
2020-01-07 16:15:53 +0700

    edit admin layout

--- mediatheque-laravel/resources/views/admin/layout.blade.php
+++ mediatheque-laravel/resources/views/admin/layout.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
@@ -21,10 +25,14 @@
                         <i id="camera_icon" class="fas fa-camera"></i>
                     </a>
                 </div>
-                <div class="text-center sideber-text mt-3 mb-2">
+                <div class="text-center sideber-text mt-3 mb-0">
                     <div style="font-size: 18px; font-weight: bold;">Oh Ri Jin</div>
-                    <div style="font-style: italic;">Camerwoman</div>
-                    <div><u>oh.ri.gin@gmail.com</u></div>
+                    <div id="positionInfo" style="font-style: italic; display: none;">Camerwoman</div>
+                    <div id="emailInfo" style="display: none;"><u>oh.ri.gin@gmail.com</u></div>
+
+                    <div id="btnShowMoreInfo" title="show more profile information">
+                        <i class="fas fa-caret-down"></i>
+                    </div>
                 </div>
             </div>
 
@@ -76,29 +84,30 @@
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
 
-                    <button type="button" id="sidebarCollapse" class="btn btn-info">
-                        <i class="fas fa-align-left"></i>
+                    <button type="button" id="sidebarCollapse" class="btn btn-info rounded rounded-circle d-flex align-items-center align-self-center">
+                        <i class="fa fa-chevron-right"></i>
                     </button>
-                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <i class="fas fa-align-justify"></i>
-                    </button>
+                    <div style="margin-left: 5px; font-size: 18px;">Mediatheque</div>
+{{--                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">--}}
+{{--                        <i class="fas fa-align-justify"></i>--}}
+{{--                    </button>--}}
+{{--                    <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
+{{--                        <ul class="nav navbar-nav ml-auto">--}}
+{{--                            <li class="nav-item active">--}}
+{{--                                <a class="nav-link" href="#">Page</a>--}}
+{{--                            </li>--}}
+{{--                            <li class="nav-item">--}}
+{{--                                <a class="nav-link" href="#">Page</a>--}}
+{{--                            </li>--}}
+{{--                            <li class="nav-item">--}}
+{{--                                <a class="nav-link" href="#">Page</a>--}}
+{{--                            </li>--}}
+{{--                            <li class="nav-item">--}}
+{{--                                <a class="nav-link" href="#">Page</a>--}}
+{{--                            </li>--}}
+{{--                        </ul>--}}
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="nav navbar-nav ml-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Page</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Page</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Page</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Page</a>
-                            </li>
-                        </ul>
-                    </div>
+{{--                    </div>--}}
                 </div>
             </nav>
 
@@ -114,6 +123,25 @@
     $(document).ready(function () {
         $('#sidebarCollapse').on('click', function () {
             $('#sidebar').toggleClass('active');
+        });
+
+        // button show more info
+        $('#btnShowMoreInfo').click(function () {
+            if($('#btnShowMoreInfo > i').attr('class') === 'fas fa-caret-down'){
+                $('#btnShowMoreInfo').attr('title', 'hide profile information');
+
+                $('#positionInfo').attr('style', 'font-style: italic; display: block;');
+                $('#emailInfo').attr('style', 'display: block;');
+                $('#btnShowMoreInfo > i').attr('class', 'fas fa-caret-up');
+            }else{
+                $('#btnShowMoreInfo').attr('title', 'show more profile information');
+
+                $('#positionInfo').attr('style', 'font-style: italic; display: none;');
+                $('#emailInfo').attr('style', 'display: none;');
+                $('#btnShowMoreInfo > i').attr('class', 'fas fa-caret-down');
+            }
+
+
         });
     });
 </script>
